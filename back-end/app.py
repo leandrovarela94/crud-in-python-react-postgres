e72c3b46777af62f4ab45893411eb3e5bb13176c
leandrovarela94 <leandro_varela@live.com>
2023-01-16 04:17:01 -0300

    Alfa - Inicialized CRUD in Python - Update project

---
+++ back-end/app.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Contact(BaseModel):
+    id: Optional[int]
+    name: str
+    phone: str
+    email: str
+
+
+@app.get("/contacts")
+def read_root(name):
+    response = name
+    return response
