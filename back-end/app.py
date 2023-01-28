b2931667a7aea2612779374a8c883fec6853a8c0
leandrovarela94 <leandro_varela@live.com>
2023-01-28 01:48:49 -0300

    Alfa - Inicialized CRUD in Python - Created Services - Created Connect - Created Models

--- back-end/app.py
+++ back-end/app.py
@@ -1,46 +1,43 @@
 from fastapi import FastAPI, Query
-from pydantic import BaseModel
-from sql.postgres import Postgres
+from models.contact import Contact
+from services.contact_services import ContactSevices
 
 app = FastAPI()
 
 
 @app.get("/contacts/")
 async def read_all():
-    query = 'SELECT * FROM list_contacts'
-    result = Postgres.get_contacts_postgres(query)
+    result = ContactSevices.get_contact_postgres()
 
     return result
 
 
-@app.get("/contacts/{id}")
-async def read_one(id: int):
-    query = f"SELECT * FROM list_contacts WHERE id ={id}"
-    result = Postgres.get_contacts_postgres(query)
+# @app.get("/contacts/{id}")
+# async def read_one(id: int):
 
-    return result
+#     result = ContactSevices.get_one_contacts_postgres(id)
+
+#     return result
 
 
 @app.post("/contacts/")
-async def create_contact(name: str, phone: str, email: str):
-    query = f"INSERT INTO list_contacts (name,phone,email) VALUES('{name}','{phone}','{email}')"
-    Postgres.post_contacts_postgres(query)
-    print(query)
-    return {f"Sucess : response:"}
+def create_contact(contact: Contact):
+
+    ContactSevices.post_contacts_postgres(contact)
+
+    return {f"Sucess : response: {contact}"}
 
 
-@app.delete("/contacts/{id}")
-async def delete_contact(id: int):
-    id_query: str = id
-    query = f"DELETE FROM list_contacts WHERE id = {id_query} ;"
-    Postgres.post_delete_postgres(query)
-    print(query)
-    return {f"Sucess : response:"}
+# @app.delete("/contacts/{id}")
+# def delete_contact(id: int):
+
+#     x = ContactSevices.delete_contact_postgres(id)
+#     return {f"Sucess : response:{id}"}
 
 
-@app.put("/contacts/{id}")
-async def update_contact(name: str, phone: str, email: str, id: int):
-    query = f"UPDATE list_contacts set name = '{name}', phone = '{phone}', email = '{email}' WHERE id = ({id}) ;"
-    Postgres.post_update_postgres(query)
-    print(query)
-    return {f"Sucess : response:"}
+# @app.put("/contacts/{id}")
+# def update_contact(contact: Contact, id: int):
+
+#     x = ContactSevices.update_contact_postgres(
+#         contact, id)
+#     return {f"Sucess : response:{contact} and id : {id}"}
---
+++ back-end/services/contact_services.py
@@ -0,0 +1,43 @@
+from models.contact import Contact
+from pydantic import BaseModel
+from sql.postgres import Postgres
+
+cur, conn = Postgres.connect_postgres()
+
+
+class ContactSevices(BaseModel):
+
+    def get_contact_postgres():
+        query = 'SELECT * FROM list_contacts'
+
+        cur.execute(query)
+
+        return cur.fetchall()
+
+    def get_one_contacts_postgres(id):
+        query = f"SELECT * FROM list_contacts WHERE id ={id}"
+
+        cur.execute(query)
+
+        return cur.fetchall()
+
+    def post_contacts_postgres(contact: Contact, Body=(...)):
+
+        query = f"INSERT INTO list_contacts (name,phone,email) VALUES('{contact.name}','{contact.phone}','{contact.email}')"
+
+        cur.execute(query)
+
+        conn.commit()
+
+    def delete_contact_postgres(id: int):
+
+        cur.execute("DELETE FROM list_contacts WHERE id = (%s) ", (id))
+
+        conn.commit()
+
+    def update_contact_postgres(contact, id):
+
+        cur.execute(
+            "UPDATE list_contacts set name = %s, phone = %s, email = %s WHERE id = %s ", (contact.name, contact.phone, contact.email, id))
+
+        conn.commit()
